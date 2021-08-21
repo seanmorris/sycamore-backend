66c11690b368b43e8a9d33002f114b2c203a8077
Sean Morris <640101+seanmorris@users.noreply.github.com>
2021-08-21 03:27:42 -0400

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,19 @@
+#!make
+.PHONY: init
+
+SHELL    = /bin/bash
+PROJECT  ?=sycamore
+REPO     ?=repository
+
+-include .env
+-include .env.${TARGET}
+
+-include vendor/seanmorris/ids/Makefile
+
+init: ${TARGET_COMPOSE}
+	@ docker run --rm \
+		-v $$PWD:/app \
+		-v $${COMPOSER_HOME:-$$HOME/.composer}:/tmp \
+		composer -vvv require seanmorris/ids:dev-master
+	@ make -s
+	@ make -s start-fg
---
+++ source/Root.php
@@ -0,0 +1,95 @@
+<?php
+namespace SeanMorris\Sycamore;
+
+use \SeanMorris\Ids\Routable;
+
+class Root implements Routable
+{
+	public function actor()
+	{
+		$publicKeyFile = 'file://' . IDS_ROOT . '/data/local/ssl/ids_rsa.pub.pem';
+		$publicKey     = file_get_contents($publicKeyFile);
+
+		return json_encode([
+			'@context' => ['https://www.w3.org/ns/activitystreams', 'https://w3id.org/security/v1'],
+		
+			'preferredUsername' => 'seanmorris',
+			'id'    => 'https://pub.seanmorr.is/actor',
+			'type'  => 'Person',
+			'inbox' => 'https://pub.seanmorr.is/inbox',
+			
+			'publicKey' => [
+				'publicKeyPem' => $publicKey,
+				'owner'        => 'https://pub.seanmorr.is/actor',
+				'id'           => 'https://pub.seanmorr.is/actor#main-key',
+			]
+		]);
+	}
+
+	public function webfinger()
+	{
+		return json_encode([
+			"subject" => "acct:sean@seanmorr.is",
+			"links"   => [[
+				"type" => "application/activity+json",
+				"href" => "https://pub.seanmorr.is/actor",
+				"rel"  => "self",
+			]]
+		]);
+	}
+
+	public function sendMessage()
+	{
+		$now = gmdate('D, d M Y H:i:s T');
+		$key = '';
+		$to  = 'seanmorris@mastodon.social';
+
+		$url     = 'https://mastodon.social/inbox';
+		$timeout = 3;
+
+		$requestTarget = sprintf('(request-target) post /inbox
+host: mastodon.social
+date: %s
+'
+			, $now
+		);
+
+		$privateKeyFile = 'file://' . IDS_ROOT . '/data/local/ssl/ids_rsa.pem';
+		$publicKeyFile  = 'file://' . IDS_ROOT . '/data/local/ssl/ids_rsa.pub.pem';
+
+		$privateKey = openssl_pkey_get_private($privateKeyFile);
+		$publicKey  = openssl_pkey_get_public($publicKeyFile);
+
+		openssl_sign($requestTarget, $signature, $privateKey);
+
+		$signatureBase64  = base64_encode($signature);
+
+		$context = stream_context_create($contextSource = ['http' => [
+			'method'    => 'POST'
+			, 'header' => [
+				'Content-Type: application/json'
+				, 'Signature: ' . $signatureBase64
+				, 'Host: mastodon.social'
+				, 'Date: ' . $now
+			]
+			, 'content' => json_encode([
+				'@context' => 'https://www.w3.org/ns/activitystreams'
+				, 'id'     => 'https://pub.seanmorr.is/create-hello-world'
+				, 'type'   => 'Create'
+				, 'actor'  => 'https://pub.seanmorr.is/actor'
+				, 'object' => [
+					'id'             => 'https://pub.seanmorr.is/hello-world'
+					, 'type'         => 'Note'
+					, 'published'    => ''
+					, 'attributedTo' => 'https://pub.seanmorr.is/actor'
+					, 'content'      => '<b>Hello, world!</b>'
+					, 'to'           => 'https://www.w3.org/ns/activitystreams#Public'
+				]
+			])
+		]]);
+
+		$handle = fopen($url, 'r', FALSE, $context);
+		fpassthru($handle);
+		fclose($handle);
+	}
+}
