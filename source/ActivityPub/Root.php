781fa10787d0b347ddef7eb07c7f73cf749ab240
Sean Morris <640101+seanmorris@users.noreply.github.com>
2021-08-28 02:40:39 -0400

    Followers

--- source/ActivityPub/Root.php
+++ source/ActivityPub/Root.php
@@ -123,7 +123,7 @@ digest: %s', $host, $now, $hash);
 			]
 		]]);
 
-		$body    = file_get_contents($url, FALSE, $context);
+		$body    = print_r(file_get_contents($url, FALSE, $context));
 		$headers = print_r($http_response_header, 1) . PHP_EOL;
 
 		return $headers . $body;
