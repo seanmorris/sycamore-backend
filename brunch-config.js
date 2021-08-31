a6abea24ace4e882d53552023e30c2e771b8db57
Sean Morris <640101+seanmorris@users.noreply.github.com>
2021-08-31 07:31:17 -0400

    JS Loader.

---
+++ brunch-config.js
@@ -0,0 +1,55 @@
+const { exec } = require('child_process');
+
+exports.files = {
+	javascripts: {joinTo: 'app.js'}
+	, stylesheets: {joinTo: 'app.css'}
+}
+
+exports.plugins = {
+	babel: {
+		presets:   ['@babel/preset-env']
+		, plugins: ["@babel/plugin-proposal-class-properties"]
+	}
+	, raw: {
+		pattern: /\.html$/,
+		wrapper: content => `module.exports = ${JSON.stringify(content)}`
+	}
+}
+
+
+exports.watcher = {
+	awaitWriteFinish: true
+};
+
+exports.hooks = {
+	preCompile: () => {
+		console.log('About to compile...');
+		exec(
+			`cd ./curvature-2 && npm link && cd ../ && npm link curvature`
+			, (err, stdout, stderr)=>{
+				console.log(err);
+				console.log(stdout);
+				console.log(stderr);
+
+				return Promise.resolve();
+			}
+		)
+	}
+};
+exports.npm = {
+	styles: {
+		codemirror: [
+			"lib/codemirror.css",
+			"theme/elegant.css",
+		]
+	}
+};
+
+exports.paths = {
+	public: './docs'
+	, watched: ['./frontend']
+};
+
+exports.modules = {
+	nameCleaner: path => path.replace(/^frontend\//, '')
+};
---
+++ infra/docker/brunch.Dockerfile
@@ -0,0 +1,23 @@
+ARG TAG
+
+FROM debian:bullseye-20210816-slim AS base
+MAINTAINER Sean Morris <sean@seanmorr.is>
+
+RUN set -eux; \
+	apt-get update; \
+	apt-get install -y gnupg apt-transport-https; \
+	curl -sL https://deb.nodesource.com/setup_14.x | bash - ; \
+	apt update; \
+	apt install -y nodejs npm; \
+	npm i -g brunch;
+
+
+WORKDIR "/app"
+CMD ["brunch", "watch", "-sn"]
+
+RUN npm install
+
+FROM base AS test
+FROM base AS dev
+FROM base AS web
+FROM base AS worker
